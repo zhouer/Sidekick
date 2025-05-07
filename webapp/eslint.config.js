77d39cf390ad47ebc44c0e90d90d4625f35db9f6
En-Jan Chou <zhouer@gmail.com>
2025-05-07 12:11:05 -0400

    Update packages

--- webapp/eslint.config.js
+++ webapp/eslint.config.js
@@ -14,11 +14,9 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
