02428807676260695004eb9de850d181825b22c3
En-Jan Chou <zhouer@gmail.com>
2025-06-09 20:55:02 -0400

    Fix vite config and change webapp and extension version to 0.0.8

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -7,19 +7,28 @@ const appVersion = JSON.stringify(process.env.npm_package_version || 'unknown');
 export default defineConfig(({ mode }) => {
   let wsHost, wsPort, wsUrl;
   let scriptUrl;
-  const plugins = [react()];
+  let plugins = [react()];
+  let headers: Record<string, string> = {};
 
-  if (mode === 'development') {
+  if (mode === 'development' || mode === 'vscode') {
     wsHost = 'localhost';
     wsPort = 5163;
     wsUrl = JSON.stringify(`ws://${wsHost}:${wsPort}`);
     scriptUrl = JSON.stringify('http://localhost:5173');
-
-    const websocketServerPlugin = require('./vite-plugin-ws-server').default;
-    plugins.push(websocketServerPlugin({ host: wsHost, port: wsPort }));
   } else {
     wsUrl = JSON.stringify('wss://ws-sidekick.zhouer.workers.dev');
     scriptUrl = JSON.stringify('https://script-sidekick.zhouer.workers.dev');
+  }
+
+  if (mode === 'development') {
+    const websocketServerPlugin = require('./vite-plugin-ws-server').default;
+    plugins.push(websocketServerPlugin({ host: wsHost, port: wsPort }));
+  }
+
+  if (mode !== 'vscode') {
+    // CORS headers are needed for SharedArrayBuffer used in Pyodide
+    headers['Cross-Origin-Opener-Policy'] = 'same-origin';
+    headers['Cross-Origin-Embedder-Policy'] = 'require-corp';
   }
 
   return {
@@ -30,10 +39,7 @@ export default defineConfig(({ mode }) => {
       '__SCRIPT_URL__': scriptUrl,
     },
     server: {
-      headers: { // CORS headers are needed for SharedArrayBuffer used in Pyodide
-        'Cross-Origin-Opener-Policy': 'same-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp',
-      },
+      headers: headers,
     },
   };
 })
